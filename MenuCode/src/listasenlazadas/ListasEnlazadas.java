a34aedee0cb8b15d439f997c03168454777b0b52
julio Aguilar <julioo1122@gmail.com>
2021-03-26 22:52:10 -0600

    Anexo de Vectores falta guardarlos

--- MenuCode/src/listasenlazadas/ListasEnlazadas.java
+++ MenuCode/src/listasenlazadas/ListasEnlazadas.java
@@ -21,16 +21,20 @@ public class ListasEnlazadas {
     public static void main(String[] args) {
         Set<Coleccion> setMenu = new HashSet<>();
         Lista lista = new Lista();
+        Vectores vector = new Vectores();
         int opcion = 0;
+        int limiteUsuario=0;
         String nombre;
+        String nombrev;
 
         do {
             try {
                 opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
                         "1.-Ingresa nombre del menu (Listas)\n2.-Imprimir lista"
-                        + "\n3.- Ingresa nombre del menu (coleccion) \n4.-Mostar todas las estructuras de datos \n5.-Salir ", "Menú", 3));
+                        + "\n3.- Ingresa nombre del menu (coleccion) \n4.-Ingresa vector menu \n 5.-Mostar todas las estructuras de datos \n6.-Salir ", "Menú", 3));
 
-                switch (opcion) {
+                switch (opcion) 
+                {
                     case 1:
                      try {
                         nombre = JOptionPane.showInputDialog("Ingresa el nombre: ", 3);
@@ -39,21 +43,57 @@ public class ListasEnlazadas {
                         JOptionPane.showMessageDialog(null, "Debes ingresar un nombre ");
                     }
                     break;
+                    
                     case 2:
                         lista.imprimirLista();
                         break;
                     case 3:try {
-                        nombre = JOptionPane.showInputDialog(null,"Ingresa el nombre: ","2° Menu",3);
+                        nombre = JOptionPane.showInputDialog(null,"Ingresa el nombre: "," Menu",3);
                         creadorMenu(setMenu, nombre);//Agrega a la coleccion
                     } catch (Exception e) {
                         JOptionPane.showMessageDialog(null, "Debes ingresar un nombre ");
                     }
                         break;
+                        
+                        //Vectores
                     case 4:
+                        try 
+                    {
+                        
+                        limiteUsuario=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingresa el Limite del arreglo:","limie",3));
+                            if (limiteUsuario>0)
+                            {
+                                 vector.limite=limiteUsuario;
+                            
+                             int i =0;
+                             while(i<=limiteUsuario)
+                             {
+                                 
+                                  nombre = JOptionPane.showInputDialog(null,"Ingresa el nombre"+i +": "," Menu",3);
+                                  vector.AddElement(nombre,i);//Agrega el vector
+                                 
+                                  i++;
+                             }
+                            
+
+                        }else
+                            {
+                                 JOptionPane.showMessageDialog(null, "El limite no debe ser 0 ni negativo ");
+                                   break;
+                            }
+                           
+                    } 
+                        catch (Exception e) {
+                        JOptionPane.showMessageDialog(null, "Debes ingresar un nombredsfsd ");
+                    }
+                        break;
+                        
+                    case 5:
                         lista.imprimirLista();
                         visualizarConjunto(setMenu);
+                        vector.PrintElement();
                         break;
-                    case 5:
+                    case 6:
                         break;
                     default:
                         JOptionPane.showMessageDialog(null, "Debes ingresar una opcion valida ");
@@ -63,7 +103,7 @@ public class ListasEnlazadas {
 
             }
 
-        } while (opcion != 5);
+        } while (opcion != 6);
 
     }
 //Creaccion de un Menu con Colecciones
---
+++ MenuCode/src/listasenlazadas/Vectores.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listasenlazadas;
+
+
+/**
+ *
+ * @author julio
+ */
+public class Vectores {
+    public int limite;
+    
+  public String nombre[]= new String[limite];
+ 
+ 
+    
+    public void AddElement(String dato,int i)
+    {
+        
+      
+        System.out.println(i+dato);
+        //agregar al vector
+       
+
+    }
+  
+    public void PrintElement()
+    {
+      //  System.out.println("- "+NombreMenu);
+    
+    }
+    
+    
+
+    
+}
